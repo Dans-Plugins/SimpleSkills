8a0697fd2edb0ef78fbcb56f6be7f1a38cd40db1
dmccoystephenson <dmccoystephenson@gmail.com>
2021-12-01 16:41:27 -0700

    Optimized imports.

--- src/main/java/dansplugins/simpleskills/eventhandlers/FishingHandler.java
+++ src/main/java/dansplugins/simpleskills/eventhandlers/FishingHandler.java
@@ -1,10 +1,7 @@
 package dansplugins.simpleskills.eventhandlers;
 
 import dansplugins.simpleskills.SupportedSkill;
-import dansplugins.simpleskills.data.PersistentData;
 import dansplugins.simpleskills.eventhandlers.abs.SkillHandler;
-import dansplugins.simpleskills.objects.PlayerRecord;
-import dansplugins.simpleskills.utils.Logger;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerFishEvent;
