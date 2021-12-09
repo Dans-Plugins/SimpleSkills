589d1287339199f59f4e03b33bbe442b82c930de
dmccoystephenson <dmccoystephenson@gmail.com>
2021-12-08 17:20:19 -0700

    Changed version.

--- src/main/java/dansplugins/simpleskills/SimpleSkills.java
+++ src/main/java/dansplugins/simpleskills/SimpleSkills.java
@@ -21,7 +21,7 @@ import java.util.Arrays;
  */
 public class SimpleSkills extends AbstractPonderPlugin {
     private static SimpleSkills instance;
-    private final String version = "v1.1-alpha-6";
+    private final String version = "v1.1-alpha-7";
 
     public static SimpleSkills getInstance() {
         return instance;
