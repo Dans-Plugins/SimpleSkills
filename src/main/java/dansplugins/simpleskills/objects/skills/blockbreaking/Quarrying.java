d8ebb0e3333895d164493ce5354b391268ae63c7
dmccoystephenson <dmccoystephenson@gmail.com>
2021-12-17 23:06:51 -0700

    Added deepslate to the associated materials for the quarrying skill.

--- src/main/java/dansplugins/simpleskills/objects/skills/blockbreaking/Quarrying.java
+++ src/main/java/dansplugins/simpleskills/objects/skills/blockbreaking/Quarrying.java
@@ -1,6 +1,7 @@
 package dansplugins.simpleskills.objects.skills.blockbreaking;
 
 import dansplugins.simpleskills.enums.SupportedSkill;
+import dansplugins.simpleskills.nms.NMSVersion;
 import dansplugins.simpleskills.objects.abs.BlockBreakingSkill;
 import dansplugins.simpleskills.objects.benefits.ResourceExtraction;
 import org.bukkit.Material;
@@ -11,6 +12,8 @@ import java.util.HashSet;
  * @author Daniel Stephenson
  */
 public class Quarrying extends BlockBreakingSkill {
+    String nms = NMSVersion.getNMSVersion();
+
     public Quarrying() {
         super(SupportedSkill.QUARRYING.ordinal(), "Quarrying");
         initialize();
@@ -26,6 +29,9 @@ public class Quarrying extends BlockBreakingSkill {
         materials.add(Material.GRANITE);
         materials.add(Material.SANDSTONE);
         materials.add(Material.TERRACOTTA);
+        if (!(nms.contains("v1_13_R1") || nms.contains("v1_13_R2") || nms.contains("v1_14_R1") || nms.contains("v1_15_R1") || nms.contains("v1_16_R1") || nms.contains("v1_16_R2") || nms.contains("v1_16_R3"))) {
+            materials.add(Material.DEEPSLATE);
+        }
         super.initializeAssociatedMaterials(materials);
     }
 }
